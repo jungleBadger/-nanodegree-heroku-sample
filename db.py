1d5badca0233411ec3fa071fa0c127d7e744ad8b
Daniel Abrao <danielpaladar@gmail.com>
2017-11-06 19:21:53 -0200

    adding triple quotes notation strings to queries

--- db.py
+++ db.py
@@ -31,30 +31,33 @@ html = '''<!DOCTYPE html>
 </pre>
 '''
 
-query_1 = (
-    "SELECT articles.title, count(*) as views "
-    "FROM articles JOIN log ON log.path "
-    "like concat('%', articles.slug, '%') "
-    "WHERE log.status like '%200%' GROUP BY "
-    "articles.title, log.path ORDER BY views desc limit 3")
+query_1 = ("""
+    SELECT articles.title, count(*) AS views
+    FROM articles JOIN log ON log.path
+    like concat('%', articles.slug, '%')
+    WHERE log.status like '%200%' GROUP BY
+    articles.title, log.path ORDER BY views desc limit 3;
+    """)
 
 
-query_2 = (
-    "SELECT authors.name, count(*) as views FROM articles inner "
-    "join authors ON articles.author = authors.id JOIN log "
-    "ON log.path like concat('%', articles.slug, '%') WHERE "
-    "log.status like '%200%' group "
-    "by authors.name ORDER BY views desc")
+query_2 = ("""
+    SELECT authors.name, count(*) as views FROM articles inner
+    join authors ON articles.author = authors.id JOIN log
+    ON log.path like concat('%', articles.slug, '%') WHERE
+    log.status like '%200%' group
+    by authors.name ORDER BY views desc
+    """)
 
 
-query_3 = (
-    "SELECT day, perc FROM ("
-    "SELECT day, round((sum(requests)/(select count(*) FROM log WHERE "
-    "substring(cast(log.time as text), 0, 11) = day) * 100), 2) as "
-    "perc FROM (select substring(cast(log.time as text), 0, 11) as day, "
-    "count(*) as requests FROM log WHERE status like '%404%' GROUP BY day)"
-    "as log_percentage GROUP BY day ORDER BY perc desc) as final_query "
-    "WHERE perc >= 1")
+query_3 = ("""
+    SELECT day, perc FROM (
+    SELECT day, round((sum(requests)/(select count(*) FROM log WHERE
+    substring(cast(log.time as text), 0, 11) = day) * 100), 2) as
+    perc FROM (select substring(cast(log.time as text), 0, 11) as day,
+    count(*) as requests FROM log WHERE status like '%404%' GROUP BY day)
+    as log_percentage GROUP BY day ORDER BY perc desc) as final_query
+    WHERE perc >= 1;
+    """)
 
 
 class Handler(http.server.BaseHTTPRequestHandler):
